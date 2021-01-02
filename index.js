67d6c0cebd48bd75d6d52cd314a25979248c13b3
kazitipu <kazi.tipu.nxt@gmail.com>
2021-01-03 04:27:18 +0600

    fixed server for 1688,taobao api

--- index.js
+++ index.js
@@ -10,10 +10,29 @@ let port = process.env.PORT || 5000
 app.get('/', (req,res)=>{
     res.send('add any ali products id to the route')
 })
+app.get('/singleProduct/:productIdAndType',async (req,res)=>{
+  let productIdAndType = req.params.productIdAndType.split(',')
+  let productId = productIdAndType[0]
+  let type = productIdAndType[1]
+  _EXTERNAL_URL =`http://api24.be/${type}/index.php?route=api_tester/call&api_name=item_get&lang=en&num_iid=${productId}&is_promotion=1&key=globalbuybd.com-kazi.tipu.nxt@gmail.com-taobao-1688`
+  await axios.get(_EXTERNAL_URL)
+    .then(function (response) {
+      // handle success
+      res.send(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+     
+      res.send(error)
+    })
+    .then(function () {
+      // always executed
+  });
+})
 
  
 
-app.get('/:searchKeywordAndPage',async (req, res)=> {
+app.get('/collection/:searchKeywordAndPage',async (req, res)=> {
   let searchKeywordAndPage = req.params.searchKeywordAndPage.split(',')
   let searchKeyword = searchKeywordAndPage[0]
   console.log(searchKeyword)
@@ -38,11 +57,11 @@ app.get('/:searchKeywordAndPage',async (req, res)=> {
   });
 })
 
-
-
 app.listen(port,()=>{
     console.log(`app is listening on the port ${port}`)
 })
+
+
 
 
 
