dfea93ed448bf4989dddd73ba1f411f1b6e30f37
kazitipu <kazi.tipu.nxt@gmail.com>
2021-01-24 04:54:36 +0600

    product upload with mutler

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const request = require('request');
-const axios = require('axios');
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const request = require("request");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
-app.use(cors())
-let port = process.env.PORT || 5000
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname + ".jpg");
+  },
+});
+const upload = multer({ storage: storage });
 
-app.get('/', (req,res)=>{
-    res.send('Welcome to the api server of globalbuybd.com')
-})
-app.get('/singleProduct/:productIdAndType',async (req,res)=>{
-  let productIdAndType = req.params.productIdAndType.split(',')
-  let productId = productIdAndType[0]
-  let type = productIdAndType[1]
-  _EXTERNAL_URL =`http://api24.ch/${type}/index.php?route=api_tester/call&api_name=item_get&lang=en&num_iid=${productId}&is_promotion=1&key=globalbuybd.com-kazi.tipu.nxt@gmail.com-taobao-1688`
-  await axios.get(_EXTERNAL_URL)
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/uploads", express.static("uploads"));
+let port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the api server of globalbuybd.com");
+});
+app.get("/singleProduct/:productIdAndType", async (req, res) => {
+  let productIdAndType = req.params.productIdAndType.split(",");
+  let productId = productIdAndType[0];
+  let type = productIdAndType[1];
+  _EXTERNAL_URL = `http://api24.ch/${type}/index.php?route=api_tester/call&api_name=item_get&lang=en&num_iid=${productId}&is_promotion=1&key=globalbuybd.com-kazi.tipu.nxt@gmail.com-taobao-1688`;
+  await axios
+    .get(_EXTERNAL_URL)
     .then(function (response) {
       // handle success
-      res.send(response.data)
+      res.send(response.data);
     })
     .catch(function (error) {
       // handle error
-     
-      res.send(error)
+
+      res.send(error);
     })
     .then(function () {
       // always executed
-  });
-})
+    });
+});
 
- 
+app.get("/collection/:searchKeywordAndPage", async (req, res) => {
+  let searchKeywordAndPage = req.params.searchKeywordAndPage.split(",");
+  let searchKeyword = searchKeywordAndPage[0];
+  console.log(searchKeyword);
+  let page = searchKeywordAndPage[1];
+  console.log(page);
 
-app.get('/collection/:searchKeywordAndPage',async (req, res)=> {
-  let searchKeywordAndPage = req.params.searchKeywordAndPage.split(',')
-  let searchKeyword = searchKeywordAndPage[0]
-  console.log(searchKeyword)
-  let page = searchKeywordAndPage[1]
-  console.log(page)
-
-  console.log(req.params)
+  console.log(req.params);
   _EXTERNAL_URL = `http://api24.ch/taobao/index.php?route=api_tester/call&api_name=item_search&lang=en&q=${searchKeyword}&start_price=0&end_price=0&page=${page}&cat=0&discount_only=&sort=&page_size=&seller_info=&nick=&ppath=&imgid=&filter=&key=globalbuybd.com-kazi.tipu.nxt@gmail.com-taobao-1688`;
 
-    await axios.get(_EXTERNAL_URL)
+  await axios
+    .get(_EXTERNAL_URL)
     .then(function (response) {
       // handle success
-      res.send(response.data)
+      res.send(response.data);
     })
     .catch(function (error) {
       // handle error
-     
-      res.send(error)
+
+      res.send(error);
     })
     .then(function () {
       // always executed
-  });
-})
+    });
+});
 
-app.listen(port,()=>{
-    console.log(`app is listening on the port ${port}`)
-})
+app.post("/uploadImage/", upload.single("productImage"), (req, res, err) => {
+  res.send(req.file);
+});
 
-
-
-
-
+app.listen(port, () => {
+  console.log(`app is listening on the port ${port}`);
+});
